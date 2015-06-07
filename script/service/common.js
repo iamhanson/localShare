863e7731bc739b308a5ad31b052b67314a351d2d
Hanson <hanson@HansondeMBP.lan>
2015-06-07 10:06:11 +0800

    cc

---
+++ script/service/common.js
@@ -0,0 +1,7 @@
+/**
+* 公共模块，提供公用的函数，方法等
+*/
+var commonHelper=(function(_self){
+    //ToDo
+    return _self;
+})(window.commonHelper||{});
---
+++ script/service/index.js
@@ -0,0 +1,5 @@
+//home模块
+var home=(function(_self){
+    
+    return _self;
+})(window.home||{});
